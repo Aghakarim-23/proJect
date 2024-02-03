375df771e35e1be79f553eb68a20160c8fa88a1b
Aghakarim-dev <hemidzade1010@gmail.com>
2024-02-03 16:12:03 +0400

    test

---
+++ script.js
@@ -0,0 +1,19 @@
+const btnBar = document.getElementById("btnBar");
+const mobileMenu = document.getElementById("mobileMenu");
+btnBar.addEventListener("click", () => {
+  mobileMenu.classList.toggle("hidden");
+});
+
+let navLink = document.getElementsByClassName("nav-link");
+for (let i = 0; i < navLink.length; i++) {
+  navLink[i].addEventListener("click", () => {
+    navLink[i].classList.add("bg-gray-400");
+    for (let u = 0; u < navLink.length; u++) {
+      if (navLink[u] != navLink[i]) {
+        navLink[u].classList.remove("bg-gray-400");
+      }
+    }
+  });
+}
+
+console.log(navLink);
